313344897ee2948cdd6e522c63c12914c4868bc9
ralph-holden <142798241+ralph-holden@users.noreply.github.com>
2024-06-23 19:43:56 +0100

    Add files via upload

--- test_run.py
+++ test_run.py
@@ -4,14 +4,13 @@ Created on Sat Jun 22 10:53:49 2024
 
 @author: 44775 Ralph Holden
 """
-
 from dna_str import *
+import pickle
 
-test = dna_string_model(100, 100, 100, [-50, -50, 0], [50, -50, 0], 100)
-
-for i in range(100000):
-    test.montecarlostep()
+test = dna_string_model(100, 100, 100, [-1, -50, 0], [1, -50, 0], 100)
    
+test.do_steps(nsteps = 100000)
+    
 print()
 print('dnastr_A')
 print(test.dnastr_A)
@@ -23,7 +22,10 @@ print()
 print('for DNA A, the final end to end distance is', test.end_to_end()[0])
 print('for DNA B, the final end to end distance is', test.end_to_end()[1])
 print()
-print('total number of paired sites', test.total_adj(), '<- clearly incorrect')
+print('total number of paired sites', test.total_adj()[0])
 
+test.proj_2d(fullbox = True )
+test.proj_2d(fullbox = False)
 
-test.proj_2d()
+with open('test_simulation.dat','wb') as data_f:
+    pickle.dump(test.trajectories, data_f)
