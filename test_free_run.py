9544cd3f127355189f388ac943e127d75696a4c9
ralph-holden <142798241+ralph-holden@users.noreply.github.com>
2024-07-12 15:48:21 +0100

    Update test_free_run.py

--- test_free_run.py
+++ test_free_run.py
@@ -4,21 +4,32 @@ Created on Sat Jun 22 10:53:49 2024
 
 @author: 44775 Ralph Holden
 """
-from free_dna_model import *
-
-import numpy as np
-import matplotlib.pyplot as plt
-
+# # # IMPORTS # # #
+from free_dna_model import Vector, Bead, Strand, Simulation, np, Tuple, combinations
 import pickle
 import sys
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation, PillowWriter
 
-s = Simulation(boxlims=Vector(50,50,50), StrandA=Strand(20,Vector(-5,0,0)), StrandB=Strand(20,Vector(5,0,0)))
 
+# # # SIMULATION PARMAMETERS # # #
 # Run the Monte Carlo algorithm for given number of steps with a progress bar
-nsteps = 1000
+nsteps = 2000
+# Length of Segments
+nsegs = 30
+ystart = -nsegs/2
+# Separation (along x axis)
+sep = 10
+xstartA, xstartB = -sep/2, +sep/2
+# Box Limits
+xlim, ylim, zlim = 20, 20, 20 # from -lim to +lim 
+
+# # # INITIALISE & RUN SIMULATION # # #
+sim = Simulation(boxlims=Vector(xlim,ylim,zlim), StrandA=Strand(nsegs,Vector(xstartA,ystart,0)), StrandB=Strand(nsegs,Vector(xstartB,ystart,0)))
 
 for i, item in enumerate(range(nsteps)):
-    s.montecarlostep()
+    sim.montecarlostep_trial()
     
     length = 20
     progress = (i + 1) / nsteps
@@ -27,27 +38,25 @@ for i, item in enumerate(range(nsteps)):
     sys.stdout.write(f"\r{bar}")
     sys.stdout.flush()
     
+# save trajectories
+with open('test_simulation.dat','wb') as data_f:
+    pickle.dump([sim.trajectoryA, sim.trajectoryB], data_f)
+    
 # convert to numpy arrays
-s.trajectoryA = np.array(s.trajectoryA)
-s.trajectoryB = np.array(s.trajectoryB)    
+sim.trajectoryA = np.array(sim.trajectoryA)
+sim.trajectoryB = np.array(sim.trajectoryB)    
 
 print()
 print('Final DNA Strand A')
-print(s.trajectoryA)
+print(sim.trajectoryA[-1])
 print()
 print('Final DNA Strand B')
-print(s.trajectoryB)
-
-
-# # # animation # # #
+print(sim.trajectoryB[-1])
     
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
-
-# Assuming 's.trajectoryA' and 's.trajectoryB' are defined and contain your data
-tA = s.trajectoryA
-tB = s.trajectoryB
+# # # ANIMATION # # #
+# data
+tA = sim.trajectoryA
+tB = sim.trajectoryB
 
 # Number of frames for the animation
 num_frames = len(tA)
@@ -66,13 +75,13 @@ x1, y1, z1 = data1[0]
 x2, y2, z2 = data2[0]
 
 # Initial line plots
-line1, = ax.plot(x1, y1, z1, color='b', marker='.', label='Set 1')
-line2, = ax.plot(x2, y2, z2, color='r', marker='.', label='Set 2')
+line1, = ax.plot(x1, y1, z1, color='b', marker='.', markersize=1, label='DNA strand A')
+line2, = ax.plot(x2, y2, z2, color='r', marker='.', markersize=1, label='DNA strand B')
 
-# Set consistent axis limits
-ax.set_xlim(-50, 50)
-ax.set_ylim(-50, 50)
-ax.set_zlim(-50, 50)
+# Axis limits
+ax.set_xlim(-sim.boxlims.x, sim.boxlims.x)
+ax.set_ylim(-sim.boxlims.y, sim.boxlims.y)
+ax.set_zlim(-sim.boxlims.z, sim.boxlims.z)
 ax.legend()
 
 # Update function for the animation
@@ -91,7 +100,7 @@ def update(frame):
 ani = FuncAnimation(fig, update, frames=num_frames, interval=400, blit=False)
 
 # Save the animation as an MP4 file (uncomment to save)
-# ani.save('3d_line_animation.mp4', writer='ffmpeg', fps=10)
+ani.save('3d_line_animation.gif', writer=PillowWriter(fps=5))
 
 # Show the plot
 plt.show()
