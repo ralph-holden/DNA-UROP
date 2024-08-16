87513bf1595bd7bd5cdaa3d764611cdd3f430bd4
ralph-holden <142798241+ralph-holden@users.noreply.github.com>
2024-08-16 18:41:43 +0100

    Update and rename free_run to free_run.py
    New data methods

--- FreeMC/free_run.py
+++ FreeMC/free_run.py
@@ -5,7 +5,7 @@ Created on Sat Jul 11 13:06:51 2024
 @author: 44775 Ralph Holden
 """
 # # # IMPORTS # # #
-from free_model import Vector, Bead, create_strand, Strand, Simulation, np, Tuple, combinations
+from free_model import Vector, Bead, Start_position, Strand, Simulation, np, Tuple, combinations
 from free_model import kb, temp, kappab, lp
 import pickle
 import sys
@@ -17,29 +17,34 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 
 # # # SIMULATION PARMAMETERS # # #
 # Run the Monte Carlo algorithm for given number of steps with a progress bar
-nsteps = 100
+nsteps = 1000
 # Length of Segments, where each segment/grain is 1/5 helical coherence length
-coherence_lengths = 11
+coherence_lengths = 20
 nsegs = 5 * coherence_lengths
 ystart = -nsegs/5/2
 # Separation (along x axis)
-sep = 0.5
+sep = 2
+sep += 0.2 # for surface to surface separation
 xstartA, xstartB = -sep/2, +sep/2
 # Box Limits
-xlim, ylim, zlim = 6, 6, 6 # from -lim to +lim 
+xlim, ylim, zlim = 30, 30, 30 # from -lim to +lim 
 
 # # # DATA OUTPUT PARAMETERS # # #
 # save data?
 save_data = False
-log_update = 10 # how often to publish values to the log file
+log_update = 50 # how often to publish values to the log file
 # animation?
 animate = True
-frame_hop = 10 # frame dump frequency
+frame_hop = 50 # frame dump frequency
 
 
 # # # INITIALISE & RUN SIMULATION # # #
-Strand1 = create_strand(nsegs,xstartA,ystart,0)
-Strand2 = create_strand(nsegs,xstartB,ystart,0)
+spA = Start_position(nsegs, xstartA, ystart, 0)
+Strand1 = spA.create_strand_straight() # spA.create_strand_curved()
+
+spB = Start_position(nsegs, xstartB, ystart, 0)
+Strand2 = spB.create_strand_straight() # spB.create_strand_curved()
+
 sim = Simulation(boxlims=Vector(xlim,ylim,zlim), StrandA=Strand1, StrandB=Strand2)
 
 
@@ -65,10 +70,16 @@ for i, item in enumerate(range(nsteps)):
         logging.info(f'''Step {i} : DATA:
 Strand A end to end = {endtoendA} lc
 Strand B end to end = {endtoendB} lc
-Simulation Total Energy = {sim.energy} kbT
+Total Pairs = {sim.pair_counts[-1][0]}
+Number of Islands = {sim.n_islands[-1]}
+Simulation Internal Energy = {sim.eng_traj[-1]}
+Island Pair Distance = {sim.av_R_islands[-1]}
+Average Island Length = {sim.av_L_islands[-1]}
+Average Island Separation = {sim.av_sep_islands[-1]}
 ...''')
         print(f'''\rStrand A end to end = {endtoendA} lc
 Strand B end to end = {endtoendB} lc
+Total Pairs = {sim.pair_counts[-1][0]}
 Simulation Total Energy = {sim.energy} kbT
 ...''')
     
@@ -88,8 +99,8 @@ endtoendB = []
 for t in range(len(sim.trajectoryA)):
     a, b = sim.endtoend(t)
     endtoendA.append(a), endtoendB.append(b)
-totpair = np.array(sim.pair_count)[:,0]
-selfpair = np.array(sim.pair_count)[:,1]
+totpair = np.array(sim.pair_counts)[:,0]
+selfpair = np.array(sim.pair_counts)[:,1]
 
 # plotting end to end distances
 endtoendendA, endtoendendB = sim.endtoend(-1)
@@ -109,10 +120,11 @@ plt.savefig('./Data_outputs/endtoend.png')
 plt.show()
 
 # plotting number of pairs
-totpair_end, selfpair_end = sim.count_tot()
+totpair_end, selfpair_end = sim.pair_counts[-1][0], sim.pair_counts[-1][1]
 print()
 print(f'Total number of paired grains = {totpair_end}')
 print(f'Number of paired grains to self = {selfpair_end}')
+print(f'Number of Islands = {sim.n_islands[-1]}')
 
 plt.figure()
 plt.title('Coarse Grain DNA Pairs')
@@ -120,6 +132,7 @@ plt.xlabel('Monte Carlo Step')
 plt.ylabel('Paired DNA Grains, $0.2 l_c$')
 plt.plot(xsteps, totpair, label = 'Total')
 plt.plot(xsteps, selfpair, label = 'Self')
+plt.plot(xsteps, sim.n_islands, label = 'Islands')
 plt.grid(linestyle=':')
 plt.legend(loc='best')
 plt.savefig('./Data_outputs/pairs.png')
@@ -127,16 +140,58 @@ plt.show()
 
 # plotting total free energy
 print()
-print(f'Free Energy = {sim.energy} kbT')
+print(f'Internal Energy = {sim.energy} kbT')
 
 plt.figure()
-plt.title('Coarse Grain DNA Free Energy')
+plt.title('Coarse Grain DNA Internal Energy')
 plt.xlabel('Monte Carlo Step')
-plt.ylabel('Free Energy, $k_bT$')
+plt.ylabel('Internal Energy, $k_bT$')
 plt.plot(xsteps, sim.eng_traj, label='')
 plt.grid(linestyle=':')
 plt.legend(loc='best')
 plt.savefig('./Data_outputs/energy.png')
+plt.show()
+
+# plotting average island pair distance
+print()
+print(f'Average Island Pair Distance = {sim.av_R_islands[-1]}')
+
+plt.figure()
+plt.title('Average Island Pair Distance')
+plt.xlabel('Monte Carlo Step')
+plt.ylabel('R, $l_c$')
+plt.plot(xsteps, sim.av_R_islands, label='')
+plt.grid(linestyle=':')
+plt.legend(loc='best')
+plt.savefig('./Data_outputs/island_R.png')
+plt.show()
+
+# plotting average island length
+print()
+print(f'Average Island Length = {sim.av_L_islands[-1]}')
+
+plt.figure()
+plt.title('Average Island Length')
+plt.xlabel('Monte Carlo Step')
+plt.ylabel('L, $l_c$')
+plt.plot(xsteps, sim.av_L_islands, label='')
+plt.grid(linestyle=':')
+plt.legend(loc='best')
+plt.savefig('./Data_outputs/island_length.png')
+plt.show()
+
+# plotting average island separation
+print()
+print(f'Average Island Separation = {sim.av_sep_islands[-1]}')
+
+plt.figure()
+plt.title('Average Island Separation')
+plt.xlabel('Monte Carlo Step')
+plt.ylabel('L, $l_c$')
+plt.plot(xsteps, sim.av_sep_islands, label='')
+plt.grid(linestyle=':')
+plt.legend(loc='best')
+plt.savefig('./Data_outputs/island_separation.png')
 plt.show()
     
 # # # ANIMATION # # #
@@ -150,7 +205,7 @@ if animate:
     num_frames = len(selected_frames)
     
     # Create a figure and a 3D axis
-    fig = plt.figure()
+    fig = plt.figure(figsize=[16,10])
     ax = fig.add_subplot(111, projection='3d')
     
     # Precomputed data (replace these lists with your actual data)
@@ -165,6 +220,9 @@ if animate:
     # Initial line plots
     line1, = ax.plot(x1, y1, z1, color='b', marker='.', markersize=1, label='DNA strand A')
     line2, = ax.plot(x2, y2, z2, color='r', marker='.', markersize=1, label='DNA strand B')
+    
+    # Create a text label for the frame number, initially set to the first frame
+    frame_text = ax.text2D(0.05, 0.95, f"Frame: {selected_frames[0]}", transform=ax.transAxes)
     
     # Axis limits
     ax.set_xlim(-sim.boxlims.x, sim.boxlims.x)
@@ -182,13 +240,16 @@ if animate:
         line1.set_data_3d(x1, y1, z1)
         line2.set_data_3d(x2, y2, z2)
         
-        return line1, line2
+        # Update the frame number text
+        frame_text.set_text(f"Frame: {selected_frames[frame]}")
+
+        return line1, line2, frame_text
     
     # Create the animation
-    ani = FuncAnimation(fig, update, frames=num_frames, interval=400, blit=False)
+    ani = FuncAnimation(fig, update, frames=num_frames, interval=50, blit=False)
     
     # Save the animation as an MP4 file (uncomment to save)
-    ani.save('./Data_outputs/3d_line_animation.gif', writer=PillowWriter(fps=1))
+    ani.save('./Data_outputs/3d_line_animation.gif', writer=PillowWriter(fps=20))
     
     logging.info('Animation saved as GIF')
     
