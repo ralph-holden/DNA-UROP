1ea6d40136d01b7d0c519f0a29e43cebf7d20f83
ralph-holden <142798241+ralph-holden@users.noreply.github.com>
2024-08-29 19:39:40 +0100

    Update free_model.py

--- FreeMC/free_model.py
+++ FreeMC/free_model.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jul 11 13:06:51 2024
 
-@author: 44775 Ralph Holden
+@author: Ralph Holden
 
 MODEL:
     polymer bead model - Worm-like-Chain, including excluded volume and specialised dsDNA interactions
@@ -26,6 +26,16 @@ import matplotlib.pyplot as plt
 from typing import Tuple
 from itertools import combinations
 
+from Vector_classholder import Vector
+
+import sys
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+target_dir = os.path.join(script_dir, '../Electrostatics_functions/')
+sys.path.insert(0, os.path.abspath(target_dir))
+
+from Electrostatics_classholder import Electrostatics
 
 
 # # # UNITS # # #
@@ -128,276 +138,8 @@ class Start_position:
         ax.set_zlabel('Z axis')
     
         plt.show()
-        
-
-
-# # # vector class # # #
-# for maths
-class Vector():
-    '''3D vectors'''
-
-    def __init__(self,i1,i2,i3):
-        '''Initialise vectors with x and y and z coordinates'''
-        self.x = i1
-        self.y = i2
-        self.z = i3
-        self.arr = np.array([self.x,self.y,self.z])
-
-    def __add__(self, other):
-        '''Use + sign to implement vector addition'''
-        return (Vector(self.x+other.x,self.y+other.y,self.z+other.z))
-
-    def __sub__(self, other):
-        '''Use - sign to implement vector "subtraction"'''
-        return (Vector(self.x-other.x,self.y-other.y,self.z-other.z))
-
-    def __mul__(self, number: float):
-        '''Use * sign to multiply a vector by a scaler on the left'''
-        return Vector(self.x*number,self.y*number,self.z*number)
-
-    def __rmul__(self, number):
-        '''Use * sign to multiply a vector by a scaler on the right'''
-        return Vector(self.x*number,self.y*number,self.z*number)
-
-    def __truediv__(self, number):
-        '''Use / to multiply a vector by the inverse of a number'''
-        return Vector(self.x/number,self.y/number,self.z/number)
-
-    def __repr__(self):
-        '''Represent a vector by a string of 3 coordinates separated by a space'''
-        return '{x} {y} {z}'.format(x=self.x, y=self.y, z=self.z)
-
-    def copy(self):
-        '''Create a new object which is a copy of the current.'''
-        return Vector(self.x,self.y,self.z)
-
-    def dot(self, other):
-        '''Calculate the dot product between two 3D vectors'''
-        return self.x*other.x + self.y*other.y + self.z*other.z
-
-    def norm(self):
-        '''Calculate the norm of the 3D vector'''
-        return (self.x**2+self.y**2+self.z**2)**0.5
     
-    def angle(self, other):
-        '''Calculate the angle between 2 vectors using the dot product'''
-        return np.arccos(self.dot(other) / (self.norm()*other.norm()))
-    
-    def cartesian_to_spherical(self):
-        """
-        Convert Cartesian coordinates to spherical coordinates.
-        
-        Parameters:
-        x (float): The x-coordinate in Cartesian coordinates.
-        y (float): The y-coordinate in Cartesian coordinates.
-        z (float): The z-coordinate in Cartesian coordinates.
-        
-        Returns:
-        tuple: A tuple containing the spherical coordinates (r, theta, phi).
-        """
-        #r = np.sqrt(x**2 + y**2 + z**2)
-        r = np.linalg.norm([self.x,self.y,self.z]) # causing errors with smaller box sizes??
-        theta = np.arctan2(self.y, self.x)
-        phi = np.arccos(self.z / r)
-        return r, theta, phi
 
-    def spherical_to_cartesian(self, r, theta, phi):
-        """
-        Convert spherical coordinates to Cartesian coordinates.
-        
-        Parameters:
-        r (float or np.ndarray): The radius in spherical coordinates.
-        theta (float or np.ndarray): The azimuthal angle in spherical coordinates (in radians).
-        phi (float or np.ndarray): The polar angle in spherical coordinates (in radians).
-        
-        Returns:
-        tuple: A tuple containing the Cartesian coordinates (x, y, z).
-               If r, theta, and phi are arrays, x, y, and z will also be arrays.
-        """
-        x = r * np.sin(phi) * np.cos(theta)
-        y = r * np.sin(phi) * np.sin(theta)
-        z = r * np.cos(phi)
-        return Vector(x, y, z)
-
-
-
-# # # ELECTROSTATICS # # #
-class Electrostatics:
-    ''' 
-    Electrostatic helical interaction as described by Kornyshev - Leikin theory
-    From 'Sequence Recognition in the Pairing of DNA Duplexes', Kornyshev & Leikin, 2001, DOI: 10.1103/PhysRevLett.86.3666
-    
-    NOTE: uses real units of metres for length, but energy in kbT, therefore force in kbT per metre
-    '''
-    # constants
-    eps = 80 # ~dielectric constant water, conversion to per Angstrom^-3
-    r = 9 * 10**-10 # radius phosphate cylinder, in Angstroms 
-    sigma = 16.8 # phosphate surface change density, in micro coulombs per cm^2
-    sigma /= 10**-6 * (10**2)**2 # in coulombs per Angstrom^2
-    theta = 0.8 # fraction phosphate charge neutralised by adsorbed counterions
-    f1, f2, f3 = 0.7, 0.3, 0 # fraction of counterions on; double heix minor groove, major groove, phosphate backbone
-    debye = 7 * 10**-10 # Debye length (kappa^-1), in Angstroms
-    H = 34 * 10**-10 # Helical pitch, in Angstroms
-    lamb_c = 100 * 10**-10 # helical coherence length, in Angstroms. NOTE: more recent estimate NOT from afformentioned paper
-    coeffs = 16 * np.pi**2 * sigma**2 / eps # coefficients for 'a' terms, apply at end of calculations, for Eint. NOTE: requires a0 to have a prefactor of 1/2
-    
-    def __init__(self, homol=False):
-        self.homol = homol     
-        
-    def kappa(self, n):
-        return np.sqrt( 1/self.debye**2  +  n**2 * (2*np.pi / self.H)**2 ) # when n=0 kappa = 1/debye
-        
-    def f(self, n):
-        return self.f1 * self.theta  +  self.f2 * (-1)**n * self.theta  -  (1 - self.f3*self.theta) * np.cos(0.4 * n * np.pi)    
-
-    def nu(self, n, L):
-        x = n**2 * L / self.lamb_c
-        return ( 1 - np.exp(-x) ) / x 
-    
-    def a(self, R: float) -> tuple([float, float, float]):
-        '''
-        Finds 'a' terms for a_0, a_1, a_2 @ R
-        For a_0 term, pairwise sum from -inf to +inf approximated as -1 to 1 (including zero)
-        For gen_energy_map(), R input can be array
-        
-        NOTE: w/out coeff factor, applied only @ Eint calculation, a0 has a prefactor of 1/2
-        '''
-        a0_coeff = 1/2
-        a0_term1 = (1-self.theta)**2 * special.kn(0, R/self.debye ) / ( (1/self.debye**2) * special.kn(1, self.r/self.debye )**2 )
-        a0_term2 = 0
-        for j in range(-1,2):
-            for n in range(-1,2):
-                a0_term2 += self.f(n)**2 / self.kappa(n)**2  *  special.kn(n-j, self.kappa(n)*R)**2 * special.ivp(j, self.kappa(n)*self.r)  /  (  special.kvp(n, self.kappa(n)*self.r)**2 * special.kvp(j, self.kappa(n)*self.r)  ) 
-        a0 = a0_coeff * (a0_term1 - a0_term2)
-        a1 = self.f(1)**2 / self.kappa(1)**2 * special.kn(0, self.kappa(1)*R ) / special.kvp(1, self.kappa(1)*self.r )**2
-        a2 = self.f(2)**2 / self.kappa(2)**2 * special.kn(0, self.kappa(2)*R ) / special.kvp(2, self.kappa(2)*self.r )**2
-
-        return a0, a1, a2
-
-    def gen_energy_map(self):
-        ''' 
-        INPUTS:
-        homol : bool , controls output for homologous and non homologous sequences, default = False.
-        
-        OUTPUT:
-        Eint  : 2D np.array, electrostatic internal energy contribution, function of L & R.
-                for homol = False -- NON homologous interaction (default)
-                for homol = True  -- HOMOLOGOUS interaction
-        '''
-        Lmin = 20  * 10**-10
-        Lmax = 300 * 10**-10
-        Lstep = 20 * 10**-10 # grain diameter
-        Lrange = np.linspace( Lmin, Lmax, int((Lmax-Lmin)/(Lstep))+1 )
-    
-        Rmin = 0.9 * 10**-10
-        Rmax = 10 * 10**-10 # same as R_cut = 0.1 * 100*10**-10
-        Rstep = 0.01 * 10**-10
-        Rrange = np.linspace( Rmin, Rmax, int((Rmax-Rmin)/(Rstep))+1 )
-        
-        Lrange, Rrange = np.meshgrid(Lrange, Rrange)
-        self.Lrange = Lrange
-        self.Rrange = Rrange
-        
-        self.a0, self.a1, self.a2 = self.a(Rrange)
-
-        if not self.homol:
-            self.Eint = self.coeffs * ( self.a0  -  self.nu(1, Lrange) * self.a1 * np.cos(np.arccos(self.a1/(4*self.a2)))  +  self.nu(2, Lrange) * self.a2 * np.cos(2*np.arccos(self.a1/(4*self.a2))) ) * Lrange
-        elif self.homol:
-            self.Eint = self.coeffs * ( self.a0  -  self.a1*np.cos(np.arccos(self.a1/(4*self.a2)))  +  self.a2*np.cos(2*np.arccos(self.a1/(4*self.a2))) ) * Lrange
-        
-        self.Eint *= (4*np.pi*epsilon_0)
-        self.Eint /= (Boltzmann * 300) 
-        self.Eint *= 10**8 # multiply by l_0 for factor of L[m] to L[l_c]
-
-    def find_energy(self, Lindex: int, R: float, ishomol = False):
-        '''Finds energy of ONE L, R point'''
-        
-        Lmin = 20  * 10**-10
-        Lmax = 100000 * 10**-10
-        Lstep = 20 * 10**-10 # grain diameter
-        Lrange = np.linspace( Lmin, Lmax, int((Lmax-Lmin)/(Lstep))+1 )
-        L = Lrange[Lindex-1]
-        
-        a0, a1, a2 = self.a(R)
-
-        SP_min_factor = np.cos(np.arccos(np.clip(a1/(4*a2), -1, 1))) if abs(a2) > 10**-10 else np.cos(np.pi) # avoid dividing by zero error
-        
-        if not ishomol: 
-            Eint = self.coeffs * ( a0  -  self.nu(1, L) * a1 * SP_min_factor  +  self.nu(2, L) * a2 * SP_min_factor ) * L
-        elif ishomol:
-            Eint = self.coeffs * ( a0  -  a1 * SP_min_factor  +  a2 * SP_min_factor ) * 1
-            
-        Eint *= (4*np.pi*epsilon_0)
-        Eint /= (Boltzmann * 300) # from gaussian to in kbT units
-        # Eint *= 10**8 # multiply by l_0 for factor of L[m] to L[l_c]
-        
-        return Eint
-    
-    def find_energy_fc(self, L: float, R: float, ishomol = False):
-        '''
-        Finds energy of ONE L, R point with fully continuous (fc) inputs 
-        For use in find_force(), where dE/dRdL required
-        Additional boolean input from force() function, default NON homologous
-        '''
-        a0, a1, a2 = self.a(R)
-        
-        SP_min_factor = np.cos(np.arccos(np.clip(a1/(4*a2), -1, 1))) if abs(a2) > 10**-10 else np.cos(np.pi) # avoid dividing by zero error
-        
-        if not ishomol: 
-            Eint = self.coeffs * ( a0  -  self.nu(1, L) * a1 * SP_min_factor  +  self.nu(2, L) * a2 * SP_min_factor ) * L
-        elif ishomol:
-            Eint = self.coeffs * ( a0  -  a1 * SP_min_factor  +  a2 * SP_min_factor ) * 1
-        
-        Eint *= (4*np.pi*epsilon_0)
-        Eint /= (Boltzmann * 300) # from gaussian to in kbT units
-        # Eint *= 10**8 # for factor of L[m] to L[l_c]
-        
-        return Eint
-
-    def force(self, Lindex: int, R: float):
-        '''         
-        INPUTS:
-        Lindex: int  , index of truncated grain pairing interaction, one unit is 0.2 lamb_c. NOTE: can be a numpy array (1D)
-        R     : float, inter-grain separation.
-        
-        OUTPUT:
-        Force  : float, magnitude (including +-) of electrostatic interaction
-                 negative gradient of energy with respect to separation, R 
-        '''
-        ishomol = type(Lindex) == str # for individual interaction, allows for non homologous interactions within homologous system
-        
-        Lmin = 20  * 10**-10
-        Lmax = 100000 * 10**-10
-        Lstep = 20 * 10**-10 # grain diameter
-        Lrange = np.linspace( Lmin, Lmax, int((Lmax-Lmin)/(Lstep))+1 )
-        L = Lrange[Lindex-1] if ishomol else 1
-        
-        h = 0.0001 * 10**-10 # for differentiation by first principles
-        # mixed differentiation by first principles, NOTE: could use analytical derivative to save computational cost
-        if not ishomol:
-            dE_dRdL = ( self.find_energy_fc(L-h, R-h) + self.find_energy_fc(L+h, R+h) - self.find_energy_fc(L-h, R+h) - self.find_energy_fc(L+h, R-h) ) / ( 4*h**2 )
-            return -1*dE_dRdL
-        if ishomol:
-            dE_dR = ( self.find_energy(Lindex, R+h, ishomol) - self.find_energy(Lindex, R, ishomol) ) / h
-            return -1*dE_dR
-        
-    def plot_energy_map(self):
-        '''Must be used after gen_energy_map()'''
-        x, y = self.Lrange, self.Rrange
-        z = self.Eint
-        
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        
-        surf = ax.plot_surface(x, y, z, cmap='viridis', alpha=0.7)
-        
-        fig.colorbar(surf)
-        
-        ax.set_xlabel('L (m)')
-        ax.set_ylabel('R (m)')
-        ax.set_zlabel('Eint (kbT)')
-        
-        plt.show()
 
 # Pairing interaction energy
 elstats = Electrostatics(homol = homology_set)
