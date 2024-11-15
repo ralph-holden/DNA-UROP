05788794c0c5497d3aa3c53ebff83df96472782b
ralph-holden <142798241+ralph-holden@users.noreply.github.com>
2024-11-15 07:55:31 +0000

    Add files via upload

--- Langevin/langevin_run.py
+++ Langevin/langevin_run.py
@@ -6,7 +6,7 @@ Created on Fri Jul 19 10:20:05 2024
 """
 # # # IMPORTS # # #
 from langevin_model import Grain, Strand, Simulation, Start_position, np, Tuple, combinations
-from langevin_model import kb, temp, kappab, lp, dt_set, homology_set, xi_set
+from langevin_model import kb, temp, kappab, lp, dt_set, homology_set, zeta_set
 import pickle
 import sys
 import os
@@ -20,18 +20,27 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 
 # # # SIMULATION PARMAMETERS # # #
 # Run the Monte Carlo algorithm for given number of steps with a progress bar
-nsteps = 50000
+nsteps = 10000
+
+# Fluctuations
+fluctuation_factor_set = 1.0
+
 # Run prior energy minimisation
-do_eng_min, nsteps_eng_min = True, 5000
+do_eng_min, nsteps_eng_min = False, 5000
+# Run prior thermal equiliration
+do_therm, nsteps_therm = False, 5000
+
 # Length of Segments, where each segment/grain is 1/5 helical coherence length
 coherence_lengths = 5
 curved = False
-nsegs = 5 * coherence_lengths 
-ystart = coherence_lengths/(2*np.pi) if curved else -1*coherence_lengths*10**-8/2
+
+# Load initial configuration?
+load_data = False # will override prior energy minimisation
+load_path = './load_trajectories/Data_outputs/'
+
 # Separation, surface to surface (along x axis)
-sep = 0.23e-8
+sep = 0.205e-8
 #sep += 0.2 # augment for surface to surface
-xstartA, xstartB = -sep/2, +sep/2
 # starting shift 
 yshift = 0.0
 
@@ -39,29 +48,37 @@ yshift = 0.0
 # # # DATA OUTPUT PARAMETERS # # #
 # data output directory
 mydir = './Data_outputs/test_params/'
-if not os.path.exists(mydir):
-    os.makedirs(mydir)
+
 # save data
 save_data = False
 log_update = 1000 # how often to publish values to the log file
+
 # save pairs
 save_pairs = True
+
 # save final DNA positions
 save_final = True
 
 # terminating settings
 recall_steps = 50000
-ignore_steps = 200000 + recall_steps
+ignore_steps = 500000 + recall_steps
 std_tol = 0.01 
 
 # animation
 animate = True
-frame_hop = 100 # frame dump frequency
-xlim, ylim, zlim = 3e-8, 6e-8, 3e-8 # Box Limits, for viewing, from -lim to +lim
+frame_hop = 250 # frame dump frequency
+xlim, ylim, zlim = 1e-8, 3e-8, 1e-8 # Box Limits, for viewing, from -lim to +lim
 
 
 
 # # # INITIALISE & RUN SIMULATION # # #
+if not os.path.exists(mydir):
+    os.makedirs(mydir)
+
+nsegs = 5 * coherence_lengths 
+ystart = coherence_lengths/(2*np.pi) if curved else -1*coherence_lengths*10**-8/2
+xstartA, xstartB = -sep/2, +sep/2
+
 spA = Start_position(nsegs, xstartA, ystart+yshift, 0)
 Strand1 = spA.create_strand_curved() if curved else spA.create_strand_straight()
 
@@ -72,6 +89,15 @@ Strand1 = spA.create_strand_curved() if curved else spA.create_strand_straight()
 spB = Start_position(nsegs, xstartB, ystart, 0)
 Strand2 = spB.create_strand_curved() if curved else spB.create_strand_straight()
 
+if load_data:
+    start_posA = np.loadtxt(load_path+'final_posA.txt')
+    start_posB = np.loadtxt(load_path+'final_posB.txt')
+    start_velA = np.loadtxt(load_path+'final_velA.txt')
+    start_velB = np.loadtxt(load_path+'final_velB.txt')
+    
+    Strand1 = spA.reload_trajectory(start_posA, start_velA)
+    Strand2 = spA.reload_trajectory(start_posB, start_velB)
+    
 sim = Simulation(StrandA=Strand1, StrandB=Strand2, boxlims=np.array([xlim,ylim,zlim]))
 
 
@@ -81,7 +107,7 @@ log_filename = datetime.now().strftime(mydir+'LOG_%Y%m%d_%H%M%S.log')
 logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(message)s')
 logging.info('Simulation started')
 logging.info(f'''Simulation parameters:
-    xi       : {xi_set}
+    zeta     : {zeta_set}
     dt       : {dt_set}
     nsteps   : {nsteps}
     num parts: {nsegs}
@@ -94,21 +120,45 @@ Starting conditions:
              ''')
 
 # Run energy minimisation
-if do_eng_min:
+if do_eng_min and not load_data:
     logging_msg = f'''Running energy minimisation for {nsteps_eng_min} steps
     Initial energy: {sim.find_energy()} kbT
                  '''
     logging.info(logging_msg), print(logging_msg)
+    
     for i in range(nsteps_eng_min):
-        sim.run_step(fluctuation_factor=0.0, dt=dt_set/50, xi=20/dt_set, cl=1)
+        sim.run_step(fluctuation_factor=0.0, dt=1e-15, zeta=100*zeta_set, correlation_length=1) # zeta=20/dt_set
     logging_msg = f'''Energy minimisation complete
     Final energy: {sim.energy_traj[-1]} kbT
+                 '''
+    logging.info(logging_msg), print(logging_msg)
+    
+# Run thermal equilibration
+if do_therm and not load_data:
+    
+    total_velocity = np.zeros(3)
+    for g in sim.StrandA.dnastr+sim.StrandB.dnastr:
+        total_velocity += np.sqrt(g.velocity**2)
+    logging_msg = f'''Running thermal equilibration for {nsteps_eng_min} steps
+    RSM velocity: {total_velocity/len(sim.StrandA.dnastr+sim.StrandB.dnastr)}
+                 '''
+    logging.info(logging_msg), print(logging_msg)
+    
+    # run steps
+    for i in range(nsteps_therm):
+        sim.run_step(fluctuation_factor=1.0, dt=1e-15, zeta=0.013869510477569768, correlation_length=1) 
+
+    total_velocity = np.zeros(3)
+    for g in sim.StrandA.dnastr+sim.StrandB.dnastr:
+        total_velocity += np.sqrt(g.velocity**2)
+    logging_msg = f'''Finished thermal equilibration
+    RSM velocity (end)  : {total_velocity/len(sim.StrandA.dnastr+sim.StrandB.dnastr)}
                  '''
     logging.info(logging_msg), print(logging_msg)
 
 # Run simulation
 for i, item in enumerate(range(nsteps)):
-    sim.run_step(fluctuation_factor=1.0)
+    sim.run_step(fluctuation_factor=fluctuation_factor_set)
     
     length = 20
     progress = (i + 1) / nsteps
@@ -146,6 +196,18 @@ Number Islands           = {sim.n_islands_traj[-1]}
             finish_msg = f'STEP {i}: Simulation terminating - pairs converged'
             print(finish_msg), logging.info(finish_msg)
             break # end simulation
+       
+# temp 
+# Run energy minimisation
+if True:
+    logging_msg = '''Running energy minimisation'''
+    logging.info(logging_msg), print(logging_msg)
+    
+    for i in range(10000):
+        sim.run_step(fluctuation_factor=0.0, dt=1e-3, zeta= 2/1e-3 , correlation_length=1) # zeta=20/dt_set
+    
+    logging_msg = '''Finished energy minimisation'''
+    logging.info(logging_msg), print(logging_msg)
        
 # extracting data from trajectories
 xsteps = np.linspace(0,len(sim.trajectoryA),len(sim.trajectoryA))
@@ -345,7 +407,13 @@ if save_pairs:
     np.savetxt(mydir+'pair_data.txt', pairs_padded, fmt='%s')
     
 if save_final:
-    np.savetxt(mydir+'final_trajA', sim.trajectoryA[-1], fmt='%s')
-    np.savetxt(mydir+'final_trajB', sim.trajectoryB[-1], fmt='%s')
+    # positions
+    np.savetxt(mydir+'final_posA.txt', sim.trajectoryA[-1], fmt='%s')
+    np.savetxt(mydir+'final_posB.txt', sim.trajectoryB[-1], fmt='%s')
+    
+    # velocities
+    velocity_listA, velocity_listB = sim.save_velocities()
+    np.savetxt(mydir+'final_velA.txt', velocity_listA, fmt='%s')
+    np.savetxt(mydir+'final_velB.txt', velocity_listB, fmt='%s')
 
 logging.info('Simulation completed')
